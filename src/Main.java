9419a030ff211938a5682e79a434d87b0ea0a337
Nicolas Sweeney-Ortiz <nsweeney-ortiz@apriori.com>
2022-11-28 21:47:22 +0000

    Test Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Filename: ${NAME}
+ * Created by: ${USER} on ${DATE}
+ * Comment: UPDATE PROGRAM COMMENTS ABOUT PROGRAM HERE
+ **/
+public class Main
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello world!");
+   }
+}
