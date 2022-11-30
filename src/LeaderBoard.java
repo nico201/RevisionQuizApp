bb40766ab011b386a377e7401f58bb6096e550d5
cuscam <campbell-s102@ulster.ac.uk>
2022-11-30 21:37:58 +0000

    MC - added get() methods, simplified set() methods

--- src/LeaderBoard.java
+++ src/LeaderBoard.java
@@ -1,6 +1,6 @@
-
 /**
  * Created by Aaron McCloskey on 27/11/2022
+ * Edited by Marcus Campbell 30/11/2022 - added get() methods, simplified set() methods
  * Basic initial leaderboard which does not write to file
  * Class Name: LeaderBoard.java
  **/
@@ -10,46 +10,68 @@ public class LeaderBoard
     private static int slot1Score;
     private static String slot2Name;
     private static int slot2Score;
-    private static String slot3name;
+    private static String slot3Name;
     private static int slot3Score;
 
-    public static void printLeaderboard()
+    public static void printLeaderboard() //SHOULD THIS BE A TOSTRING() METHOD?
     {
-       System.out.println("Leader Board");
-       System.out.println("*******************");
-       System.out.println("Name\t\t\tScore");
-       System.out.println("1st: " + slot1Name + "\t\t\t" + slot1Score);
-       System.out.println("2nd: " + slot2Name + "\t\t\t" + slot2Score);
-       System.out.println("3rd: " + slot3name + "\t\t\t" + slot3Score);
+        System.out.println("Leader Board");
+        System.out.println("*******************");
+        System.out.println("Name\t\t\tScore");
+        System.out.println("1st: " + slot1Name + "\t\t\t" + slot1Score);
+        System.out.println("2nd: " + slot2Name + "\t\t\t" + slot2Score);
+        System.out.println("3rd: " + slot3Name + "\t\t\t" + slot3Score);
     }
 
-    public static void setSlot1Name(String slot1Name)
+    public static void setSlot1Name(String newSlot1Name) { slot1Name = newSlot1Name; }
+    public static String getSlot1Name()
     {
-        LeaderBoard.slot1Name = slot1Name;
+        return slot1Name;
     }
 
-    public static void setSlot1Score(int slot1Score)
+    public static void setSlot1Score(int newSlot1Score)
     {
-        LeaderBoard.slot1Score = slot1Score;
+        slot1Score = newSlot1Score;
+    }
+    public static int getSlot1Score()
+    {
+        return slot1Score;
     }
 
-    public static void setSlot2Name(String slot2Name)
+    public static void setSlot2Name(String newSlot2Name)
     {
-        LeaderBoard.slot2Name = slot2Name;
+        slot2Name = newSlot2Name;
+    }
+    public static String getSlot2Name()
+    {
+        return slot2Name;
     }
 
-    public static void setSlot2Score(int slot2Score)
+    public static void setSlot2Score(int newSlot2Score)
     {
-        LeaderBoard.slot2Score = slot2Score;
+        slot2Score = newSlot2Score;
+    }
+    public static int getSlot2Score()
+    {
+        return slot2Score;
     }
 
-    public static void setSlot3name(String slot3name)
+    public static void setSlot3name(String newSlot3name)
     {
-        LeaderBoard.slot3name = slot3name;
+        slot3Name = newSlot3name;
+    }
+    public static String getSlot3Name()
+    {
+        return slot3Name;
     }
 
-    public static void setSlot3Score(int slot3Score)
+    public static void setSlot3Score(int newSlot3Score)
     {
-        LeaderBoard.slot3Score = slot3Score;
+        slot3Score = newSlot3Score;
     }
+    public static int getSlot3Score()
+    {
+        return slot3Score;
+    }
+
 }//class
