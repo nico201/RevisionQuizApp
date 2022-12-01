e75d3010514f2304432f11eeb7275cb8b7208118
Aaron <aaronfmccloskey@hotmail.co.uk>
2022-12-01 09:49:46 +0000

    Add new question functionality added (creating question objects)

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main
     private static String passwordAttempt;
     private static int chooseNewOrExisting;
     private static int adminMenuChoice;
+    public static int addNewQuestionMenuChoice;
 
 
     public static void main(String[] args)
@@ -39,7 +40,63 @@ public class Main
                         if (adminMenuChoice == 1)
                             LeaderBoard.printLeaderboard();
                         else if (adminMenuChoice == 2)
-                            System.out.println("Add question here");
+                            do {
+                                System.out.println("\nPlease select a question type to be added: ");
+                                System.out.println("\n1. Multiple Choice Question\n2. True or False Question\n3. Short Answer Question\nor\n4. Go Back");
+                                addNewQuestionMenuChoice = keyboard.nextInt();
+                                if (addNewQuestionMenuChoice == 1) {
+                                    MultipleChoiceQuestion mcq1 = new MultipleChoiceQuestion(null, -1, null, null, null, null, null, -1);
+                                    System.out.println("\nMCQ Question Creation");
+                                    System.out.println("Please enter the question text: ");
+                                    mcq1.setQuestionText(keyboard.next());//
+                                    mcq1.setQuestionText(keyboard.nextLine());//"Workaround for quirk in Scanner class"- Aaron
+                                    System.out.println("Please enter the number of points available: ");
+                                    mcq1.setPoints(keyboard.nextInt());
+                                    System.out.println("Please enter the topic: ");
+                                    mcq1.setTopic(keyboard.next());
+                                    System.out.println("Please enter text for option 1: ");
+                                    mcq1.setOption1(keyboard.next());
+                                    System.out.println("Please enter text for option 2: ");
+                                    mcq1.setOption2(keyboard.next());
+                                    System.out.println("Please enter text for option 3: ");
+                                    mcq1.setOption3(keyboard.next());
+                                    System.out.println("Please enter text for option 4: ");
+                                    mcq1.setOption4(keyboard.next());
+                                    System.out.println("Please enter correct option number: ");
+                                    mcq1.setCorrectOption(keyboard.nextInt());
+                                    System.out.println("New Question has been saved. Thank you!");
+                                }//if
+                                if (addNewQuestionMenuChoice == 2) {
+                                    TrueFalseQuestion tf1 = new TrueFalseQuestion(null, -1, null, '0');
+                                    System.out.println("\nTrue or False Question Creation");
+                                    System.out.println("Please enter the question text: ");
+                                    tf1.setQuestionText(keyboard.next());
+                                    tf1.setQuestionText(keyboard.nextLine());
+                                    System.out.println("Please enter the number of points available: ");
+                                    tf1.setPoints(keyboard.nextInt());
+                                    System.out.println("Please enter the topic: ");
+                                    tf1.setTopic(keyboard.next());
+                                    System.out.println("Please enter answer T or F: ");
+                                    tf1.setAnswer(keyboard.next().charAt(0));
+                                    System.out.println("New Question has been saved. Thank you!");
+                                }//if
+                                if (addNewQuestionMenuChoice == 3) {
+                                    ShortQuestion sq1 = new ShortQuestion(null, 0, null, null);
+                                    System.out.println("Short Answer Question Creation");
+                                    System.out.println("Please enter the question text: ");
+                                    sq1.setQuestionText(keyboard.next());
+                                    sq1.setQuestionText(keyboard.nextLine());
+                                    System.out.println("Please enter the number of points available: ");
+                                    sq1.setPoints(keyboard.nextInt());
+                                    System.out.println("Please enter the topic: ");
+                                    sq1.setTopic(keyboard.next());
+                                    System.out.println("Please enter the correct answer");
+                                    sq1.setAnswer(keyboard.next());
+                                    System.out.println("New Question has been saved. Thank you!");
+                                }//if
+                            }//do
+                            while (addNewQuestionMenuChoice != 4);
+
                     } while (adminMenuChoice != 3);
                 }
                 else
