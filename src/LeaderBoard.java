2d00746053196bb4d6fb204adde315f06bdc4124
cuscam <campbell-s102@ulster.ac.uk>
2022-12-12 17:28:14 +0000

    Leaderboard - amended to handle small numbers of students

--- src/LeaderBoard.java
+++ src/LeaderBoard.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 /**
  * Created by Aaron McCloskey on 27/11/2022
  * Leaderboard to display students' high scores in rank order
@@ -25,44 +27,67 @@ public class LeaderBoard {
         // work through the student arraylist one by one
         for (Student studentUser : Student.studentList) {
 
-            // if their highest score is a new top score
-            if (studentUser.getHighestScore() >= leaderScores[0]) {
-                for (int index = (checkLeaderboardSize() - 1); index >= 1; index--) {
-                    leaderScores[index] = leaderScores[index - 1];
-                    leaderNames[index] = leaderNames[index - 1];
-                }//for
-                leaderScores[0] = studentUser.getHighestScore();
-                leaderNames[0] = studentUser.getUsername();
-            }//if
+            // if only 1 student exists they are automatically top of the leaderboard
+            if (checkLeaderboardSize() == 1) {
+               leaderScores[0] = studentUser.getHighestScore();
+               leaderNames[0] = studentUser.getUsername();
+            }//if (1 student)
 
-            // if somewhere in between top and bottom
-            else if ((studentUser.getHighestScore() < leaderScores[0]) && (studentUser.getHighestScore() >= leaderScores[checkLeaderboardSize() - 2])) {
-                // work from top to bottom to find out exactly where it fits
-                for (int index = 0; index <= (checkLeaderboardSize() - 2); index++) {
-                    if ((studentUser.getHighestScore() < leaderScores[index]) && (studentUser.getHighestScore() >= leaderScores[index + 1])) {
-                        // then update everything below accordingly
-                        for (int update = (checkLeaderboardSize() - 1); update >= index + 2; update--) {
-                            leaderScores[update] = leaderScores[update - 1];
-                            leaderNames[update] = leaderNames[update - 1];
+            // if only 2 students exist - decide which order they are in
+            else if (checkLeaderboardSize() == 2) {
+                if (studentUser.getHighestScore() >= leaderScores[0]) {
+                    leaderScores[1] =  leaderScores[0];
+                    leaderNames[1] = leaderNames[0];
+                    leaderScores[0] = studentUser.getHighestScore();
+                    leaderNames[0] = studentUser.getUsername();
+                }
+               else {
+                    leaderScores[1] = studentUser.getHighestScore();
+                    leaderNames[1] = studentUser.getUsername();
+               }
+            }//if 2 students
+
+            // if 3 or more students exist - more complex sorting algorithm
+            else if (checkLeaderboardSize() >= 3) {
+                    // if the student's highest score is a new top score
+                    if (studentUser.getHighestScore() >= leaderScores[0]) {
+                        for (int index = (checkLeaderboardSize() - 1); index >= 1; index--) {
+                            leaderScores[index] = leaderScores[index - 1];
+                            leaderNames[index] = leaderNames[index - 1];
                         }//for
-                        //and insert the new values
-                        leaderScores[index + 1] = studentUser.getHighestScore();
-                        leaderNames[index + 1] = studentUser.getUsername();
-                    }//if
-                }//for
-            }//if
+                        leaderScores[0] = studentUser.getHighestScore();
+                        leaderNames[0] = studentUser.getUsername();
+                    }//if new top score
 
-            // if new bottom score
-            else if ((studentUser.getHighestScore() < leaderScores[checkLeaderboardSize() - 2]) && (studentUser.getHighestScore() >= leaderScores[checkLeaderboardSize() - 1])) {
-                leaderScores[checkLeaderboardSize() - 1] = studentUser.getHighestScore();
-                leaderNames[checkLeaderboardSize() - 1] = studentUser.getUsername();
-            }//if
+                    // if somewhere in between top and bottom
+                    else if ((studentUser.getHighestScore() < leaderScores[0]) && (studentUser.getHighestScore() >= leaderScores[checkLeaderboardSize() - 2])) {
+                        // work from top to bottom to find out exactly where it fits
+                        for (int index = 0; index <= (checkLeaderboardSize() - 2); index++) {
+                            if ((studentUser.getHighestScore() < leaderScores[index]) && (studentUser.getHighestScore() >= leaderScores[index + 1])) {
+                                // then update everything below accordingly
+                                for (int update = (checkLeaderboardSize() - 1); update >= index + 2; update--) {
+                                    leaderScores[update] = leaderScores[update - 1];
+                                    leaderNames[update] = leaderNames[update - 1];
+                                }//for
+                                //and insert the new values
+                                leaderScores[index + 1] = studentUser.getHighestScore();
+                                leaderNames[index + 1] = studentUser.getUsername();
+                            }//if
+                        }//for
+                    }//if somewhere in between top and bottom
 
-        }//for
+                    // if student's score is the very bottom score
+                    else if ((studentUser.getHighestScore() < leaderScores[checkLeaderboardSize() - 2]) && (studentUser.getHighestScore() >= leaderScores[checkLeaderboardSize() - 1])) {
+                        leaderScores[checkLeaderboardSize() - 1] = studentUser.getHighestScore();
+                        leaderNames[checkLeaderboardSize() - 1] = studentUser.getUsername();
+                    }//if bottom score
+            }//if 3 or more students exist
+
+        }//for working through student arraylist
 
     }//updateLeaderboard
 
-    public static void printLeaderboard(String leaderboardUser) {
+    protected static void printLeaderboard(String leaderboardUser) {
         updateLeaderboard();
 
         // display student leaderboard which only shows selected top subset
